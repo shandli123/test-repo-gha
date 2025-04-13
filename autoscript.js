2d53366224e8974d80d3afe802714eb8f40dda79
Shandli Kamal <74612009+shandli123@users.noreply.github.com>
2025-04-13 20:07:01 +0530

    Update autoscript.js

--- autoscript.js
+++ autoscript.js
@@ -1,24 +1,15 @@
-const webdriver = require('selenium-webdriver');
+// random commit #random 62
+var webdriver = require('selenium-webdriver');
 
-// const capabilities = {
-//   browserName: 'firefox',
-//   os: 'OS X',
-//   os_version: 'Sonoma',
-//   'bstack:options' : {
-//     		 // ChromeOption to start chrome in incognito mode
-//          name: 'BStack-[NodeJS] Sample Test',
-//          build: process.env.BROWSERSTACK_BUILD_NAME,
-//          browserName: 'firefox',
-//          os: 'OS X',
-//          os_version: 'Sonoma',
-//     	}
-// };
+// Input capabilities
 var capabilities = {
   'browserName': 'chrome',
   'os': 'os x',
   'build': process.env.BROWSERSTACK_BUILD_NAME,
   'project': process.env.BROWSERSTACK_PROJECT_NAME,
   'os_version': 'high sierra',
+  'browserstack.local': 'true',
+  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
 }
@@ -34,25 +25,3 @@ driver.get('http://localhost:8099').then(function () {
     driver.quit();
   });
 });
-
-// (async function runTest() {
-//   let driver = await new Builder()
-//     .usingServer(`https://${process.env.BROWSERSTACK_USERNAME}:${process.env.BROWSERSTACK_ACCESS_KEY}@hub.browserstack.com/wd/hub`)
-//     .withCapabilities(capabilities)
-//     .build();
-
-//   try {
-//     console.log('Started execution');
-//     await driver.get('https://www.google.com/');
-//     await driver.get('https://www.twitter.com');
-//     console.log('Finished execution');
-    
-//     await new Promise(resolve => setTimeout(resolve, 60000));
-//   } catch (error) {
-//     await driver.executeScript(
-//       'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some elements failed to load"}}'
-//     );
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
