53e17ebb6a50569ae223341a6c27c43eea1d1b67
Shandli Kamal <74612009+shandli123@users.noreply.github.com>
2025-04-13 20:14:13 +0530

    Update autoscript.js

--- autoscript.js
+++ autoscript.js
@@ -3,17 +3,24 @@ var webdriver = require('selenium-webdriver');
 
 // Input capabilities
 var capabilities = {
-  'browserName': 'chrome',
-  'os': 'os x',
-  'build': process.env.BROWSERSTACK_BUILD_NAME,
-  'project': process.env.BROWSERSTACK_PROJECT_NAME,
-  'os_version': 'high sierra',
-  'browserstack.user': process.env.BROWSERSTACK_USERNAME,
-  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
+  browserName: 'firefox',
+  os: 'OS X',
+  os_version: 'Sonoma',
+  'bstack:options' : {
+    		 // ChromeOption to start chrome in incognito mode
+         name: 'BStack-[NodeJS] Sample Test',
+         build: process.env.BROWSERSTACK_BUILD_NAME,
+        project: process.env.BROWSERSTACK_PROJECT_NAME,
+         browserName: 'firefox',
+         os: 'OS X',
+         os_version: 'Sonoma',
+    	}
+  //'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+  //'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
 }
 
 var driver = new webdriver.Builder()
-  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+  .usingServer('https://${process.env.BROWSERSTACK_USERNAME}:${process.env.BROWSERSTACK_ACCESS_KEY}@hub.browserstack.com/wd/hu')
   .withCapabilities(capabilities)
   .build();
 
